4a0a1d1be52a26b855b505acc22ba80c9ed305f8
John Hampton <john_j_hampton@yahoo.com>
2015-06-13 15:45:43 -0400

    Define passing functions for first for tests in arrays.js

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,50 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
+    var location;
+    var indexChecked;
 
+    for (indexChecked = 0; indexChecked < arr.length; indexChecked++) {
+      if (item === arr[indexChecked]) {
+        location = indexChecked;
+         break;
+      }
+    }
+    if (typeof location === 'undefined') {
+      location = -1;
+    }
+    return location;
   },
 
   sum : function(arr) {
-
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove : function(arr, item) {
+    var newArray;
 
+    newArray = arr.filter(function(el) {
+    return (el !== item);
+  });
+
+    return newArray;
   },
 
   removeWithoutCopy : function(arr, item) {
 
+
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+
+    return arr;
   },
 
   append : function(arr, item) {
